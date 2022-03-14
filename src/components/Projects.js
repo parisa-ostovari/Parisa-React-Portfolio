616b29ff69a13c242649431341d35a28f64edb0f
Parisa Ostovari <parisaostovari@gmail.com>
2022-03-14 01:41:18 -0700

    corrected links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -63,7 +63,7 @@ export default function Work() {
 
         {/* Covid Travel Guide Project */}
         <div className="text-slate-800 sm:w-fit md:w-fit w-screen p-4 md:p-0">
-          <a target="_blank" rel="noreferrer" href="https://floating-fjord-17758.herokuapp.com/"><img
+          <a target="_blank" rel="noreferrer" href="https://intense-mesa-45226.herokuapp.com/"><img
             className="border-slate-100 border-4 hover:border-cyan-800 rounded-lg hover:opacity-80 hover:border-4 md:mb-4 duration-300"
             src={ Travel } alt="Covid Travel Guide" title="Covid Travel Guide" /></a>
         </div>
@@ -83,7 +83,7 @@ export default function Work() {
             <li className="mr-4"> BootstrapCSS</li>
           </ul>
           <div className="flex mt-3 float-right">
-            <a target="_blank" rel="noreferrer" href="https://github.com/parisa-ostovari/RateMyPortfolio"><svg
+            <a target="_blank" rel="noreferrer" href="https://github.com/parisa-ostovari/CovidTravelGuide"><svg
               className="h-7 w-7 fill-orange-700 hover:stroke-cyan-800 hover:fill-cyan-800 rounded-full duration-300"
               viewBox="0 0 128 128">
               <title> GitHub </title>
@@ -95,7 +95,7 @@ export default function Work() {
                 d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm2.446 2.729c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zM31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm3.261 3.361c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm4.5 1.951c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm4.943.361c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm4.598-.782c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0">
               </path>
             </svg></a>
-            <a target="_blank" rel="noreferrer" href="https://floating-fjord-17758.herokuapp.com/"><svg xmlns="http://www.w3.org/2000/svg"
+            <a target="_blank" rel="noreferrer" href="https://intense-mesa-45226.herokuapp.com/"><svg xmlns="http://www.w3.org/2000/svg"
               className="h-7 w-7 ml-2 hover:stroke-cyan-800 duration-300 stroke-orange-700" viewBox="0 0 24 24" fill="none">
               <title> Deployed Application </title>
               <desc> Deployed Application </desc>
